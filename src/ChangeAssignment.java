632849c69dcad2c65bfa47a3f6ff21f8fa8f4cd6
mbake94 <mbake94@gmail.com>
2020-04-20 13:30:55 -0700

    first commit

---
+++ src/ChangeAssignment.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ChangeAssignment {
+    public static void main(String[]args){
+        Scanner keyboard = new Scanner(System.in);
+
+        int num1, num2, num3;
+
+        System.out.print( "First temperature? " );
+        num1 = keyboard.nextInt();
+
+        System.out.print( "Second temperature? " );
+        num2 = keyboard.nextInt();
+
+        num3 = (num1+num2)/2;
+
+        System.out.println("The average temperature is: " + num3);
+
+    }
+}
